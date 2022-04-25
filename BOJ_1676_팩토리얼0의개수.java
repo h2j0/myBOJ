f48197e80a53e6df35cd9e91a310ca45c8c0714d
h2j0 <heejeong0527@gmail.com>
2022-04-26 01:30:50 +0900

    Update BOJ_1676_팩토리얼0의개수.java

--- BOJ_1676_팩토리얼0의개수.java
+++ BOJ_1676_팩토리얼0의개수.java
@@ -3,34 +3,23 @@ package BOJ;
 import java.util.Scanner;
 
 public class BOJ_1676_팩토리얼0의개수 {
-	static int nfact;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		nfact = 1;
+		// 10의 배수가 몇개있는지 cnt
+		// 10 = 2*5 => 2의 배수 개수와 5의 배수 개수 중 min값이 답
+		// 근데 5의배수보다 2의배수가 무조건 많으니까 5의 배수만 고려해줘도 된다
 		int cnt = 0;
-		if (n != 0) {
-			// 팩토리얼한 결과
-			long factRes = fact(n);
-			// 스트링으로 만들어주고
-			String strNum = "" + factRes;
-			// 뒤에서부터 0개수 세자
-			for (int i = strNum.length() - 1; i >= 0; i--) {
-				if (strNum.charAt(i) == '0')
-					cnt++;
-				else
-					break;
+
+		for (int i = 1; i <= n; i++) {
+			int tmp = i;
+			while (tmp % 5 == 0) {
+				cnt++;
+				tmp = tmp / 5;
 			}
 		}
 		System.out.println(cnt);
 	}
 
-	private static int fact(int n) {
-		if (n == 1) {
-			return 1;
-		}
-		nfact = n * fact(n - 1);
-		return nfact;
-	}
 }
