1455673b17a5f2cd8f4cb997b99dc6294e4c648e
h2j0 <heejeong0527@gmail.com>
2022-04-28 01:41:40 +0900

    220428

---
+++ BOJ_1927_최소힙.java
@@ -0,0 +1,34 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1927_최소힙 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+				
+		for(int i=0;i<n;i++) {
+			int num = Integer.parseInt(br.readLine());
+			// 들어오는 숫자가 0이면
+			if(num==0) {
+				// 프큐가 비어있다면 출력값에 0 넣기
+				if(pq.isEmpty()) {
+					sb.append("0").append("\n");
+				}else {
+					// 뭔가 있으면 빼내
+					sb.append(pq.poll()).append("\n");
+				}
+			}else { // 들어오는 수가 0이 아니라면 프큐에 담아
+				pq.add(num);
+			}
+			
+		}
+		System.out.println(sb);
+	}
+}
