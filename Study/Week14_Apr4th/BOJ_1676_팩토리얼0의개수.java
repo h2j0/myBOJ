c411476377cceb8b9335401c7fa3d55bd688c13a
h2j0 <heejeong0527@gmail.com>
2022-04-28 22:04:16 +0900

    Apr4th

--- Study/Week14_Apr4th/BOJ_1676_팩토리얼0의개수.java
+++ Study/Week14_Apr4th/BOJ_1676_팩토리얼0의개수.java
@@ -1,4 +1,4 @@
-package BOJ;
+package BOJ.Study.Week14_Apr4th;
 
 import java.util.Scanner;
 
--- Study/Week14_Apr4th/BOJ_7569_토마토.java
+++ Study/Week14_Apr4th/BOJ_7569_토마토.java
@@ -1,4 +1,4 @@
-package BOJ;
+package BOJ.Study.Week14_Apr4th;
 
 import java.io.BufferedReader;
 import java.io.IOException;
